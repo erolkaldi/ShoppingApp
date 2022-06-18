65e70b910ab5b246b294e128252d08e72fc6c2e7
erolkaldi <43037084+erolkaldi@users.noreply.github.com>
2022-06-18 21:17:28 +0300

    Add project files.

---
+++ Identity/ShoppingApp.IdentityApi/Controllers/AuthController.cs
@@ -0,0 +1,15 @@
+﻿
+namespace ShoppingApp.IdentityApi.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult GetToken()
+        {
+            return Ok("Working");
+        }
+    }
+}
