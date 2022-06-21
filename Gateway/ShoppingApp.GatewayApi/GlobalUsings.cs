eba9ed626529129bb726d6bfeee1d1e1ab428456
erolkaldi <43037084+erolkaldi@users.noreply.github.com>
2022-06-21 22:42:32 +0300

    Ocelot Gateway api

---
+++ Gateway/ShoppingApp.GatewayApi/GlobalUsings.cs
@@ -0,0 +1,5 @@
+﻿global using Microsoft.AspNetCore.Authentication.JwtBearer;
+global using Microsoft.IdentityModel.Tokens;
+global using Ocelot.DependencyInjection;
+global using Ocelot.Middleware;
+global using System.Text;
